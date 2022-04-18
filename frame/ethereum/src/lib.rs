79ed3f29475bfdfdd4dfbf6a0643dceeff66f355
tgmichel <telmo@purestake.com>
2022-04-18 17:54:38 +0200

    Skip balance and gas price checks on non-transactional calls (#57)
    * Skip balance and gas price checks on non-transactional calls
    
    (cherry picked from commit b7081b45eea3c2f512137c83e11e90200feee3a1)
    
    * Update comment
    
    (cherry picked from commit caf6566de575832c909f2ed5f6741174304b0874)
    
    * Ensure account nonce first
    
    (cherry picked from commit 81abaddcf03cf39f603ad15578477e3b25f465b3)

--- frame/ethereum/src/lib.rs
+++ frame/ethereum/src/lib.rs
@@ -796,6 +796,7 @@ impl<T: Config> Pallet<T> {
 			}
 		};
 
+		let is_transactional = true;
 		match action {
 			ethereum::TransactionAction::Call(target) => {
 				let res = T::Runner::call(
@@ -808,6 +809,7 @@ impl<T: Config> Pallet<T> {
 					max_priority_fee_per_gas,
 					nonce,
 					access_list,
+					is_transactional,
 					config.as_ref().unwrap_or(T::config()),
 				)
 				.map_err(Into::into)?;
@@ -824,6 +826,7 @@ impl<T: Config> Pallet<T> {
 					max_priority_fee_per_gas,
 					nonce,
 					access_list,
+					is_transactional,
 					config.as_ref().unwrap_or(T::config()),
 				)
 				.map_err(Into::into)?;
