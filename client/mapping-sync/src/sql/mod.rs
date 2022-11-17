1a0ffa71933d5c56f008ab0c664c7658f5c9ee38
tgmichel <telmo@purestake.com>
2022-11-17 12:10:30 +0100

    More logs

--- client/mapping-sync/src/sql/mod.rs
+++ client/mapping-sync/src/sql/mod.rs
@@ -92,6 +92,10 @@ where
 		loop {
 			futures::select! {
 				_ = (&mut import_interval).fuse() => {
+					log::debug!(
+						target: "frontier-sql",
+						"🕐  New interval"
+					);
 					let leaves = backend.leaves();
 					if let Ok(mut leaves) = leaves {
 						if let Some(hash) = resume_at {
@@ -119,6 +123,10 @@ where
 					import_interval.reset(interval);
 				},
 				notification = notifications.next() => if let Some(notification) = notification {
+					log::debug!(
+						target: "frontier-sql",
+						"📣  New notification"
+					);
 					// On first notification try create indexes
 					if try_create_indexes {
 						try_create_indexes = false;
