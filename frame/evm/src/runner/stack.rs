6912d6098a4d677b01e65a8f18a46110863f8d5e
Éloïs <c@elo.tf>
2024-10-18 16:53:28 +0200

    refund pov gas based on measured proof size usage

--- frame/evm/src/runner/stack.rs
+++ frame/evm/src/runner/stack.rs
@@ -29,6 +29,8 @@ use evm::{
 	gasometer::{GasCost, StorageTarget},
 	ExitError, ExitReason, ExternalOperation, Opcode, Transfer,
 };
+// Cumulus
+use cumulus_primitives_storage_weight_reclaim::get_proof_size;
 // Substrate
 use frame_support::{
 	traits::{
@@ -79,6 +81,7 @@ where
 		is_transactional: bool,
 		weight_limit: Option<Weight>,
 		proof_size_base_cost: Option<u64>,
+		measured_proof_size_before: u64,
 		f: F,
 	) -> Result<ExecutionInfoV2<R>, RunnerError<Error<T>>>
 	where
@@ -109,6 +112,7 @@ where
 			weight,
 			weight_limit,
 			proof_size_base_cost,
+			measured_proof_size_before,
 		);
 
 		#[cfg(feature = "forbid-evm-reentrancy")]
@@ -147,6 +151,7 @@ where
 				weight,
 				weight_limit,
 				proof_size_base_cost,
+				measured_proof_size_before,
 			)
 		});
 
@@ -168,6 +173,7 @@ where
 		weight: Weight,
 		weight_limit: Option<Weight>,
 		proof_size_base_cost: Option<u64>,
+		measured_proof_size_before: u64,
 	) -> Result<ExecutionInfoV2<R>, RunnerError<Error<T>>>
 	where
 		F: FnOnce(
@@ -296,15 +302,23 @@ where
 					None => 0,
 				};
 
-				let pov_gas = match executor.state().weight_info() {
-					Some(weight_info) => weight_info
+				// Measure actual proof size usage (or get computed proof size)
+				let actual_proof_size = if let Some(measured_proof_size_after) = get_proof_size() {
+					measured_proof_size_after.saturating_sub(measured_proof_size_before)
+				} else {
+					executor
+						.state()
+						.weight_info()
+						.unwrap_or_default()
 						.proof_size_usage
 						.unwrap_or_default()
-						.saturating_mul(T::GasLimitPovSizeRatio::get()),
-					None => 0,
 				};
 
 				// Post execution.
+				let pov_gas = core::cmp::min(
+					actual_proof_size.saturating_mul(T::GasLimitPovSizeRatio::get()),
+					gas_limit,
+				);
 				let used_gas = executor.used_gas();
 				let effective_gas = U256::from(core::cmp::max(
 					core::cmp::max(used_gas, pov_gas),
@@ -477,6 +491,7 @@ where
 		proof_size_base_cost: Option<u64>,
 		config: &evm::Config,
 	) -> Result<CallInfo, RunnerError<Self::Error>> {
+		let measured_proof_size_before = get_proof_size().unwrap_or_default();
 		if validate {
 			Self::validate(
 				source,
@@ -506,6 +521,7 @@ where
 			is_transactional,
 			weight_limit,
 			proof_size_base_cost,
+			measured_proof_size_before,
 			|executor| executor.transact_call(source, target, value, input, gas_limit, access_list),
 		)
 	}
@@ -525,6 +541,7 @@ where
 		proof_size_base_cost: Option<u64>,
 		config: &evm::Config,
 	) -> Result<CreateInfo, RunnerError<Self::Error>> {
+		let measured_proof_size_before = get_proof_size().unwrap_or_default();
 		if validate {
 			Self::validate(
 				source,
@@ -554,6 +571,7 @@ where
 			is_transactional,
 			weight_limit,
 			proof_size_base_cost,
+			measured_proof_size_before,
 			|executor| {
 				let address = executor.create_address(evm::CreateScheme::Legacy { caller: source });
 				T::OnCreate::on_create(source, address);
@@ -580,6 +598,7 @@ where
 		proof_size_base_cost: Option<u64>,
 		config: &evm::Config,
 	) -> Result<CreateInfo, RunnerError<Self::Error>> {
+		let measured_proof_size_before = get_proof_size().unwrap_or_default();
 		if validate {
 			Self::validate(
 				source,
@@ -610,6 +629,7 @@ where
 			is_transactional,
 			weight_limit,
 			proof_size_base_cost,
+			measured_proof_size_before,
 			|executor| {
 				let address = executor.create_address(evm::CreateScheme::Create2 {
 					caller: source,
@@ -640,6 +660,7 @@ where
 		config: &evm::Config,
 		contract_address: H160,
 	) -> Result<CreateInfo, RunnerError<Self::Error>> {
+		let measured_proof_size_before = get_proof_size().unwrap_or_default();
 		if validate {
 			Self::validate(
 				source,
@@ -669,6 +690,7 @@ where
 			is_transactional,
 			weight_limit,
 			proof_size_base_cost,
+			measured_proof_size_before,
 			|executor| {
 				T::OnCreate::on_create(source, contract_address);
 				let (reason, _) = executor.transact_create_force_address(
