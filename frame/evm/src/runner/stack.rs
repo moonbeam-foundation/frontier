c9a6134b9fc6f26e2b122c4169e17f9a812e3d37
Tarek Mohamed Abdalla <tarekkma@gmail.com>
2024-10-24 12:43:57 +0000

    + Merge pull request #224 from moonbeam-foundation/elois-extcodesize-shoul EXTCODESIZE should read only once + account_code_metadata return zero size is no metadata Required to be compatible with b7d6e27 (Merge pull request #224 from moonbeam-foundation/elois-extcodesize-should-read-once) + fix: contracts without metadata must be considered EOA

--- frame/evm/src/runner/stack.rs
+++ frame/evm/src/runner/stack.rs
@@ -221,7 +221,8 @@ where
 		//
 		// EIP-3607: https://eips.ethereum.org/EIPS/eip-3607
 		// Do not allow transactions for which `tx.sender` has any code deployed.
-		if is_transactional && !<AccountCodes<T>>::get(source).is_empty() {
+		let account_code_metadata = <AccountCodesMetadata<T>>::get(source);
+		if is_transactional && account_code_metadata.unwrap_or_default().size != 0 {
 			return Err(RunnerError {
 				error: Error::<T>::TransactionMustComeFromEOA,
 				weight,
@@ -1011,7 +1012,6 @@ impl<'vicinity, 'config, T: Config> SubstrateStackState<'vicinity, 'config, T> {
 		address: H160,
 		weight_info: &mut WeightInfo,
 		recorded: &mut Recorded,
-		create_contract_limit: u64,
 	) -> Result<(), ExitError> {
 		let maybe_record = !recorded.account_codes.contains(&address);
 		// Skip if the address has been already recorded this block
@@ -1020,26 +1020,17 @@ impl<'vicinity, 'config, T: Config> SubstrateStackState<'vicinity, 'config, T> {
 			// Transfers to EOAs with standard 21_000 gas limit are able to
 			// pay for this pov size.
 			weight_info.try_record_proof_size_or_fail(IS_EMPTY_CHECK_PROOF_SIZE)?;
-			if <AccountCodes<T>>::decode_len(address).unwrap_or(0) == 0 {
-				return Ok(());
-			}
 
+			// We record metadata read as well
 			weight_info.try_record_proof_size_or_fail(ACCOUNT_CODES_METADATA_PROOF_SIZE)?;
+
 			if let Some(meta) = <AccountCodesMetadata<T>>::get(address) {
 				weight_info.try_record_proof_size_or_fail(meta.size)?;
-			} else {
-				weight_info.try_record_proof_size_or_fail(create_contract_limit)?;
-
-				let actual_size = Pallet::<T>::account_code_metadata(address).size;
-				if actual_size > create_contract_limit {
-					fp_evm::set_storage_oog();
-					return Err(ExitError::OutOfGas);
-				}
-				// Refund unused proof size
-				weight_info.refund_proof_size(create_contract_limit.saturating_sub(actual_size));
 			}
+
 			recorded.account_codes.push(address);
 		}
+
 		Ok(())
 	}
 }
@@ -1112,7 +1103,11 @@ where
 	}
 
 	fn code(&self, address: H160) -> Vec<u8> {
-		<AccountCodes<T>>::get(address)
+		if AccountCodesMetadata::<T>::contains_key(address) {
+			<AccountCodes<T>>::get(address)
+		} else {
+			Default::default()
+		}
 	}
 
 	fn storage(&self, address: H160, index: H256) -> H256 {
@@ -1302,12 +1297,6 @@ where
 	}
 
 	fn record_external_operation(&mut self, op: evm::ExternalOperation) -> Result<(), ExitError> {
-		let size_limit: u64 = self
-			.metadata()
-			.gasometer()
-			.config()
-			.create_contract_limit
-			.unwrap_or_default() as u64;
 		let (weight_info, recorded) = self.info_mut();
 
 		if let Some(weight_info) = weight_info {
@@ -1316,7 +1305,7 @@ where
 					weight_info.try_record_proof_size_or_fail(ACCOUNT_BASIC_PROOF_SIZE)?
 				}
 				ExternalOperation::AddressCodeRead(address) => {
-					Self::record_address_code_read(address, weight_info, recorded, size_limit)?;
+					Self::record_address_code_read(address, weight_info, recorded)?;
 				}
 				ExternalOperation::IsEmpty => {
 					weight_info.try_record_proof_size_or_fail(IS_EMPTY_CHECK_PROOF_SIZE)?
@@ -1335,7 +1324,7 @@ where
 					}
 				}
 				ExternalOperation::DelegationResolution(address) => {
-					Self::record_address_code_read(address, weight_info, recorded, size_limit)?;
+					Self::record_address_code_read(address, weight_info, recorded)?;
 				}
 			};
 		}
@@ -1377,12 +1366,6 @@ where
 			_ => None,
 		};
 
-		let size_limit: u64 = self
-			.metadata()
-			.gasometer()
-			.config()
-			.create_contract_limit
-			.unwrap_or_default() as u64;
 		let (weight_info, recorded) = self.info_mut();
 
 		if let Some(weight_info) = weight_info {
@@ -1401,16 +1384,6 @@ where
 
 					if let Some(meta) = <AccountCodesMetadata<T>>::get(address) {
 						weight_info.try_record_proof_size_or_fail(meta.size)?;
-					} else if let Some(remaining_proof_size) = weight_info.remaining_proof_size() {
-						let pre_size = remaining_proof_size.min(size_limit);
-						weight_info.try_record_proof_size_or_fail(pre_size)?;
-
-						let actual_size = Pallet::<T>::account_code_metadata(address).size;
-						if actual_size > pre_size {
-							return Err(ExitError::OutOfGas);
-						}
-						// Refund unused proof size
-						weight_info.refund_proof_size(pre_size.saturating_sub(actual_size));
 					}
 
 					Ok(())
