d00fdd7ffa2a7847bc4bf18237a5aae247be4e6d
Tarek Mohamed Abdalla <tarekkma@gmail.com>
2025-03-25 02:04:12 +0200

    Add suggestions

--- frame/evm/src/lib.rs
+++ frame/evm/src/lib.rs
@@ -682,21 +682,26 @@ pub mod pallet {
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
 		fn on_initialize(_: BlockNumberFor<T>) -> Weight {
-			let pov_before = get_proof_size();
+			let mut total_weight = Weight::zero();
 
-			let zero_account: H160 = H160::zero();
+			// Do dummy read to populate the pov with the intermediates nodes,
+			// only when proof size recording is enabled.
+			if let Some(pov_before) = get_proof_size() {
+				const ZERO_ACCOUNT: H160 = H160::zero();
 
-			// just a dummy read to populate the pov with the intermediates nodes
-			let _ = AccountCodesMetadata::<T>::get(zero_account.clone());
-			let (_, min_gas_weight) = T::FeeCalculator::min_gas_price();
-			let (_, account_basic_weight) = Pallet::<T>::account_basic(&zero_account);
+				// just a dummy read to populate the pov with the intermediates nodes
+				let _ = AccountCodesMetadata::<T>::get(ZERO_ACCOUNT.clone());
+				let (_, min_gas_weight) = T::FeeCalculator::min_gas_price();
+				let (_, account_basic_weight) = Pallet::<T>::account_basic(&ZERO_ACCOUNT);
 
-			let pov = get_proof_size().unwrap_or_default() - pov_before.unwrap_or_default();
+				let pov = get_proof_size().unwrap_or_default() - pov_before;
 
-			let total_weight = Weight::from_parts(0, pov)
-				+ T::DbWeight::get().reads(1)
-				+ account_basic_weight
-				+ min_gas_weight;
+				let total_weight = T::DbWeight::get().reads(1)
+					.saturating_add(Weight::from_parts(0, pov))
+					.saturating_add(account_basic_weight)
+					.saturating_add(min_gas_weight);
+
+			}
 
 			total_weight
 		}
