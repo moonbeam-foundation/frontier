6727696bb3b64c69542c273ba1e691ec850423a7
Artur Gontijo <arturgontijo@gmail.com>
2026-01-15 15:06:53 -0300

    expose lru_cache

--- client/rpc/src/cache/mod.rs
+++ client/rpc/src/cache/mod.rs
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-mod lru_cache;
+pub mod lru_cache;
 
 use std::{
 	collections::{BTreeMap, HashMap},
